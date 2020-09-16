0865be0925dbff46df91007f247b00a1ee6495b9
Henrik <hh222kt@student.lnu.se>
2020-09-16 09:29:00 +0200

    initial commit fpr c# console application

---
+++ BoatClub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoatClub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
