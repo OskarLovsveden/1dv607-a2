c75d4227d96c9ba4a72d2ff7bf68657c7014d9fe
Henrik <hh222kt@student.lnu.se>
2020-09-30 15:29:51 +0200

    Adding prompt-class instance in Register-class for collecting userinput

--- BoatClub/View/Register.cs
+++ BoatClub/View/Register.cs
@@ -8,6 +8,7 @@ namespace View
     public class Register
     {
         private Model.MemberList _memberList;
+        private View.Prompt _prompt = new View.Prompt();
 
         public MenuItems MenuItems { get; set; }
         public Register(Model.MemberList memberList)
@@ -35,29 +36,27 @@ namespace View
 
         private string SetMemberName()
         {
-            System.Console.WriteLine("Enter members name");
-            Regex rgx = new Regex("^[a-zA-Z]{1,100}$");
-            string name;
-
-            do
-            {
-                System.Console.WriteLine("Name can only contain 1-100 letters");
-                name = Console.ReadLine();
-            } while (!rgx.IsMatch(name));
-
-            return name;
+            return _prompt.PromptQuestion(
+                    "Enter members name",
+                    "Name can only contain 1-100 letters",
+                    (string name) =>
+                    {
+                        Regex rgx = new Regex("^[a-zA-Z]{1,100}$");
+                        return !rgx.IsMatch(name);
+                    }
+                );
         }
         private Model.PersonalID SetMemberPid()
         {
-            System.Console.WriteLine("Enter members Personal ID");
-            Regex rgx = new Regex(@"^[0-9]{6}[-]{1}[0-9]{4}$");
-            string PID;
-            do
-            {
-                Console.WriteLine("\nValid format: YYMMDD-XXXX");
-                PID = Console.ReadLine();
-
-            } while (!rgx.IsMatch(PID));
+            string PID = _prompt.PromptQuestion(
+                    "Enter members Personal ID",
+                    "Valid format: YYMMDD-XXXX",
+                    (string pid) =>
+                    {
+                        Regex rgx = new Regex(@"^[0-9]{6}[-]{1}[0-9]{4}$");
+                        return !rgx.IsMatch(pid);
+                    }
+                );
 
             return new Model.PersonalID(PID);
         }
